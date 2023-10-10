95d233774018c88fdd3bff89f9ca23240e4d1531
JoaoVitorDiass <joovitordiasdasilva@gmail.com>
2023-10-09 22:27:44 -0300

    feat: adicao de cores aos comandos

--- funcaoSecretaNaoOlha.h
+++ funcaoSecretaNaoOlha.h
@@ -30,4 +30,30 @@ void iniciarAParada() {
 
 }
 
-//Kuase Linux
+enum cores 
+{   
+    DARKBLUE = 1, 
+    DARKGREEN = 2,
+    LIGHTBLUE = 3,
+    RED = 4,
+    PURPLE = 5,
+    YELLOW = 6,
+    WHITE = 7,
+    GRAY = 8,
+    BLUE = 9,
+    GREEN = 10
+};
+
+void textcolor (int color)
+{
+    static int __BACKGROUND;
+
+    HANDLE h = GetStdHandle ( STD_OUTPUT_HANDLE );
+    CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+
+
+    GetConsoleScreenBufferInfo(h, &csbiInfo);
+
+    SetConsoleTextAttribute (GetStdHandle (STD_OUTPUT_HANDLE),
+                            color + (__BACKGROUND << 4));
+}
